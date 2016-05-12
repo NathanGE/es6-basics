c6fb0ed583b1da0fd20a44e16f5cf981d7863d7b
Nathan Elliott <nathangelliott@gmail.com>
2016-05-12 02:54:35 +0000

    Added an example of using import and export

---
+++ adder.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const DIGITS = 10;
+function adder(x){
+  return DIGITS + x;
+}
+
+// Prior to use make sure you have run 
+// npm install --save-dev babel-preset-es2015
+export default adder;
---
+++ runner.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// To run the file in the console run the command
+// babel-node --presets es2015 runner.js
+import adder from './adder';
+
+console.log(adder(2));
